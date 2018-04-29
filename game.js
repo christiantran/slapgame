12beb7cf210eb02884c73f5406af31a4fd07a1a8
Christian Tran <christiantran04@gmail.com>
2018-04-29 13:45:23 -0600

    end of day

--- game.js
+++ game.js
@@ -1,32 +1,58 @@
-var health = 100;
-var targetName = 'Frank';
-var hits = 0;
+//var health = 100;
+//var targetName = 'Frank the Fly';
+//var hits = 0;
+
+function Target(targetName){
+
+    this.health = 100;
+    this.targetName = name;
+    this.hits = 0;
+    this.items = []
+}
+
+var myTarget = new Target();
+// myTarget.items.push();
+
+//myTarget.items.push(items.zapper)
+var items = {
+
+    zapper: new Item("Zapper",2.5,"Electrocute those flys!"),
+    swatter: new Item("Swatter",0.5,"Swat those annoyances away!"),
+    newspaper: new Item("Newspaper",1.5,"Smash it!")
+}
+
+function Options(name, modifier, description){
+    this.name = name;
+    this.modifier = modifier;
+    this.description = description;
+}
 
 function update() {
-    document.getElementById('health').innerText = health
-    document.getElementById('hits').innerText = hits
+
+    document.getElementById('health').innerText = myTarget.health
+    document.getElementById('hits').innerText = myTarget.hits
 }
 
 update()
 
 function slap() {
-    health--
-    hits++
+    myTarget.health--
+    myTarget.hits++
     //alert(health)
 
     update()
 }
 
 function punch() {
-    health = (health-5)
-    hits++
+    myTarget.health = (myTarget.health-5)
+    myTarget.hits++
 
     update()
 }
 
 function kick() {
-    health = (health-10)
-    hits++
+    myTarget.health = (myTarget.health-10)
+    myTarget.hits++
 
     update()
 }
